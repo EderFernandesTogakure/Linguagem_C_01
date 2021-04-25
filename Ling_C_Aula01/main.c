0e1a23af8fc076d480d20f61398d0a48f9d2c84c
Eder Carlos Fernandes <eder.tecnologias@gmail.com>
2021-04-25 17:52:44 -0300

    Add files via upload

---
+++ Ling_C_Aula01/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+    printf("Meu primeiro programa na linguagem C \n");
+    printf("Digite um valor inteiro \n");
+    scanf("%d", &numero);
+    printf("O valor digitado e %d \n", numero);
+}
